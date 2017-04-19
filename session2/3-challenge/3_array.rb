53773503062384d557c0b392689e3a3e9bb6b04c
Jenny <jenny.wem@gmail.com>
2017-04-19 19:07:12 +0100

    Added solution for challenge 3, session 2

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,12 @@
 
 class String
   def every_other_char
+    result = ""
+    each_char.with_index do |char, index|
+      result << char if index.even?
   end
-  
+  print result
+  end
 end
+
+"Hello world".every_other_char
