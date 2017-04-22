71c90f504bdfea6a86c1a6b891419e1a30772832
wemmm <jenny.wem@gmail.com>
2017-04-22 12:25:46 +0100

    Added solution for challenge 4, session 3

--- session3/3-challenge/4_hashes.rb
+++ session3/3-challenge/4_hashes.rb
@@ -6,3 +6,12 @@
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
 
+def first_pos(string)
+  result = {}
+  string.split.each_with_index do |word, index|
+  result[word] ||= index
+  end
+  result
+end
+
+print first_pos("The dog and the cat and the cow")
