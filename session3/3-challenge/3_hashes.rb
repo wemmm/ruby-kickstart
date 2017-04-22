345c95881d7c6df64de8f2cc8fbd88c8654e523d
wemmm <jenny.wem@gmail.com>
2017-04-22 11:33:08 +0100

    Added solution for challenge 3, session 3

--- session3/3-challenge/3_hashes.rb
+++ session3/3-challenge/3_hashes.rb
@@ -6,3 +6,13 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def downcase_wordcount(string)
+  words = Hash.new { 0 }
+  string.split(" ").each { |word| words[word.downcase] += 1 }
+  words
+end
+
+
+print downcase_wordcount("Lovely weather we're having")
+print downcase_wordcount("How much wood would a woodchuck chuck if a woodchuck could chuck wood")
